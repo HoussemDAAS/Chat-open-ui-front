e26c76a2485f1f9a121c0afb94213d90a19bb1ab
HoussemDAAS <houssemdaas2@gmail.com>
2023-12-18 09:31:22 +0100

    navbar  done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,15 @@ import './navbar.css'
 import {RiMenu3Line,RiCloseLine} from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 const NaVbar = () => {
+  const Menu =() =>(
+    <>
+      <p><a href='#HOME'>Home</a></p>
+        <p><a href='#WhatWeeding'>About us</a></p>
+        <p><a href='#possibility'>offers</a></p>
+        <p><a href='#Features'>Feature</a></p>
+        <p><a href='#blog'>Our work </a></p>
+    </>
+  )
   const[toggleMenu,setToggleMenu]=useState(false)
   return (
     <div  className='wedding__NavBar'>
@@ -11,24 +20,30 @@ const NaVbar = () => {
         <img src={logo} alt='logo' />
       </div>
       <div className='wedding__NavBar-Links_container'>
-        <p><a href='#HOME'>Home</a></p>
-        <p><a href='#WhatWeeding'>What is Wedding Planner</a></p>
-        <p><a href='#possibility'>What we offer</a></p>
-        <p><a href='#Features'>Feature</a></p>
-        <p><a href='#blog'>Our work </a></p>
+   <Menu />
+      </div>
       </div>
       <div className='wedding__NavBar-sign'>
         <button type='button'>Contact us</button>
       </div>
-      <div
-      className='wedding__NavBar-menu'>
+      <div className='wedding__NavBar-menu'>
         {toggleMenu 
         ? <RiCloseLine color="#fff" size={27}  onClick={()=>setToggleMenu(false)} /> 
         : <RiMenu3Line color="#fff" size={27}   onClick={()=>setToggleMenu(true)} />}
+         {toggleMenu && (
+        <div className="wedding__NavBar-menu_container scale-up-center">
+          <div className="wedding__NavBar-menu_container-links">
+        <Menu />
+          </div>
+          <div className="wedding__NavBar-menu_container-links-sign">
+           <button type="button">Sign up</button>
+          </div>
+        </div>
+        )}
       </div>
-     </div>
+     
     </div>
-  )
+  );
 };
 
 export default NaVbar
